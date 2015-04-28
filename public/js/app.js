bf1699d01cc61a87b49efe6c701f1c349c8f6185
AlexKvazos <contact@alexkvazos.com>
2015-04-28 02:39:44 -0500

    More cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,44 @@
 // initialize angular application
 var app = angular.module('chat', []);
 
+
+
+/**
+ * FACTORIES
+ */
+
 // socket service
 app.factory('socket', function() {
   var socket = io(window.location.host);
   return socket;
 });
+
+
+// buffer service
+app.factory('buffer', function() {
+  return {
+    append: function(string) {
+      $('#buffer').append(string + '<br>\n');
+    },
+    error: function(error) {
+      $('#buffer').append('<span style="color:#D62D18;">' + error.errorMessage + '</span><br>')
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * CONTROLLERS
+ */
 
 // connect controller
 app.controller('connectController', function($scope, socket) {
@@ -32,21 +65,22 @@ app.controller('connectController', function($scope, socket) {
 
 });
 
+
 // buffer controller
-app.controller('bufferController', function($scope, socket) {
+app.controller('bufferController', function($scope, socket, buffer) {
 
   // initial console buffer
-  $('#buffer').append('<br>');
-  $('#buffer').append('Welcome to AlexKvazos\'s Minecraft Chat App');
-  $('#buffer').append('<br>');
-  $('#buffer').append('&gt; Press connect to connect to a Minecraft server and start chatting!');
-  $('#buffer').append('<br><br>');
-  $('#buffer').append('<i>Note: This application only works with 1.8 Minecraft servers.</i>');
-  $('#buffer').append('<br>-<br><br>');
+  buffer.append('<br>');
+  buffer.append('Welcome to AlexKvazos\'s Minecraft Chat App');
+  buffer.append('&gt; Press connect to connect to a Minecraft server and start chatting!');
+  buffer.append('<br>');
+  buffer.append('<i>Note: This application only works with 1.8 Minecraft servers.</i>');
+  buffer.append('---');
+  buffer.append('<br>');
 
+  // buffer errors when received
   socket.on('bot:error', function(error) {
-    console.log(error);
-    $('#buffer').append('<span style="color:#D62D18;">' + error.errorMessage + '</span><br>');
+    buffer.error(error);
   });
 
 });
