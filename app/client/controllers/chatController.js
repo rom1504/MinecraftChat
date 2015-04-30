b1d364a9e48cfff49924f1120864f983d6ae181f
AlexKvazos <contact@alexkvazos.com>
2015-04-30 18:35:17 -0500

    Removed spawn event emitter

--- app/client/controllers/chatController.js
+++ app/client/controllers/chatController.js
@@ -6,10 +6,6 @@ module.exports = function($scope, socket) {
 
   $scope.chat = '';
 
-  socket.on('bot:spawn', function() {
-    socket.emit('chat', { message: 'Connected via MinecraftChat - https://chat.alexkvazos.com' });
-  });
-
   $scope.send = function() {
     if ($scope.chat.trim().length > 0) {
 
--- app/server/bot/events/spawn.js
+++ app/server/bot/events/spawn.js
@@ -5,7 +5,6 @@ module.exports = (socket) => {
     var pos = socket.mcbot.entity.position;
     socket.emit('buffer:info', 'Spawned at X:' + pos.x + ', Y:' + pos.y + ', Z:' + pos.z);
     socket.emit('bot:players', socket.mcbot.players);
-    socket.emit('bot:spawn');
   });
 
 };
